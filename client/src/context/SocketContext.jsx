b224e78b1ae3f5095e1d5b04eac2cd42ffb67316
Parv Sidana <parvsidana2002@gmail.com>
2024-03-28 01:23:04 +0530

    Updated the socket url

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -17,7 +17,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (authUser) {
-            const socket = io("http://localhost:8000", {
+            const socket = io("https://friendfusion.onrender.com/", {
                 query: {
                     userId: authUser._id
                 }
